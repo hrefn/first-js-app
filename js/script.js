76f9c7b9dc5c828e8ca79d5d678fe9d64038e36f
hrefn <cmcintosh1000@gmail.com>
2022-02-27 18:45:27 -0600

    task 2.2
    creating pokemonList array and add 3 pokemon objects to it

--- js/script.js
+++ js/script.js
@@ -1,3 +1,26 @@
-alert('Hello World!');
-let favoriteFood = 'Pizza';
-document.write(favoriteFood);
+let pokemonList = [];
+
+pokemonList = [];
+
+let pokemon1 = {
+  name: 'Bulbasaur',
+  height: 0.7,
+  weight: 6.9,
+  type: ['grass', 'poison']
+};
+
+let pokemon2 = {
+  name: 'Charmander',
+  height: 0.6,
+  weight: 8.5,
+  type: ['fire']
+};
+
+let pokemon3 = {
+  name: 'Squirtle',
+  height: 0.5,
+  weight: 9,
+  type: ['water']
+};
+
+pokemonList.push(pokemon1, pokemon2, pokemon3);
