4cbd9ca66f4d61551285d7efef91c28e399a4ea0
hrefn <cmcintosh1000@gmail.com>
2022-03-14 09:13:48 -0500

    task 1.11

--- js/script.js
+++ js/script.js
@@ -1,31 +1,3 @@
-<<<<<<< Updated upstream
-let pokemonList = [];
-
-pokemonList = [];
-
-let pokemon1 = {
-  name: 'Bulbasaur',
-  height: 0.7,
-  weight: 6.9,
-  type: ['grass', 'poison']
-};
-
-let pokemon2 = {
-  name: 'Charmander',
-  height: 0.6,
-  weight: 8.5,
-  type: ['fire']
-};
-
-let pokemon3 = {
-  name: 'Squirtle',
-  height: 0.5,
-  weight: 9,
-  type: ['water']
-};
-
-pokemonList.push(pokemon1, pokemon2, pokemon3);
-=======
 /* eslint-disable no-undef */
 // initialize pokemonRepository as IIFE
 let pokemonRepository = (function () {
@@ -208,4 +180,3 @@ $('#search').on('keyup', function() {
   pokemonRepository.search($(this).val())
 })
 
->>>>>>> Stashed changes
